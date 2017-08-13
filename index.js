f1e27a93a4e1a1d482b173fae0de0eb945cc7763
Priyanka Malviya <priyankamalviya@Priyankas-MacBook-Pro.local>
2017-08-13 15:33:46 -0700

    set up twitter api data as environment variables and post tweets

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var twit = require('twit');
-
-var bot = new Twit({
+var bot = new twit({
   consumer_key: process.env.twitterbot_consumer_key,
   consumer_secret: process.env.twitterbot_consumer_secret,
   access_token: process.env.twitterbot_access_token,
-  access_token_secret: process.env.twitterbot_access_token_secret
+  access_token_secret: process.env.twitterbot_access_token_secret,
   timeout: 60 * 1000
 });
+
+
+bot.post('statuses/update', {status: 'hello world!'},
+    function(err, data, response){
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log(data.text + 'was tweeted');
+      }
+    });
